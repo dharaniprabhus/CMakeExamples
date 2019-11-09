43dccfa398468456dfddcf65f58362853e5d5372
unknown <dharani.prabhu.s@gmail.com>
2019-11-09 21:00:54 +0100

    A minimal cmake project tutorial

---
+++ app/main.cpp
@@ -0,0 +1,10 @@
+#include <exp.h>
+#include <arith.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "1 + 2 = " << add(1,2) <<std::endl;
+    std::cout << "2 * 2 = " << sqr(2) <<std::endl;
+    return 0;
+}
